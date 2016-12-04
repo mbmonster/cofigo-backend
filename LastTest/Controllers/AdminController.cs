0e80f6bd095cab858ca2817636ed5c3ac5428eef
hoanglb <mr.bin.bkdn@gmail.com>
2016-12-04 18:15:46 +0700

    loginfacebookAPI

---
+++ LastTest/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LastTest.Controllers
+{
+    public class AdminController : Controller
+    {
+        CoffeeServicesEntities db = new CoffeeServicesEntities();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListUser()
+        {
+            var listUser = db.Users.ToList();
+            return View(listUser);
+        }
+    }
+}
