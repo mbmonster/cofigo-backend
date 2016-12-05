7c5ba098f5e4f1d39888613a80977a33af617d55
Ngọc Hòa <hoa1881992@gmail.com>
2016-12-05 19:09:48 +0700

    add store

---
+++ LastTest/Controllers/StoreActionController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Web;
+using System.Web.Http.Results;
+using System.Web.Mvc;
+
+namespace LastTest.Controllers
+{
+    public class StoreActionController : Controller
+    {
+        CoffeeServicesEntities db = new CoffeeServicesEntities();
+        // GET: StoreAction
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewStore()
+        {
+            var list = (from s in db.Stores select s).ToList();
+            return View(list);
+        }
+     
+        public ActionResult AddStore()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddStore(FormCollection form, Store store)
+        {
+            string namestore = form["NameStore"];
+            string address = form["Address"];
+            string avatar = form["Avatar"];
+            string cover = form["Cover"];
+            int rep = Convert.ToInt32(form["Rep"]);
+            double lat = Convert.ToDouble(form["Latitude"]);
+            double longt = Convert.ToDouble(form["Longtitude"]);
+            store.NameStore = namestore;
+            store.Address = address;
+            store.Avatar = avatar;
+            store.Cover = cover;
+            store.Rep = rep;
+            store.Latitude = lat;
+            store.Longtitude = longt;
+            db.Stores.Add(store);
+            db.SaveChanges();
+           
+
+            return RedirectToAction("ViewStore");
+        }
+        
+        public ActionResult DeleteStore(int id)
+        {
+            var store = db.Stores.First(m => m.ID == id);
+            db.Stores.Remove(store);
+            db.SaveChanges();
+            return RedirectToAction("ViewStore");
+        }
+    }
+}
