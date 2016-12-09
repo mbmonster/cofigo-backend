d2997eaeb3ac71939bd9305dc5e1cbbb6976c2f6
Ngọc Hòa <hoa1881992@gmail.com>
2016-12-09 20:16:00 +0700

    DONE

---
+++ LastTest/Models/OrderManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LastTest.Models
+{
+    public class OrderManager : IOrderManager
+    {
+        CoffeeServicesEntities db = new CoffeeServicesEntities();
+        List<Order> listoOrders = new List<Order>();
+
+        public OrderManager()
+        {
+            var list = from s in db.Orders select s;
+            foreach (var item in list)
+            {
+                listoOrders.Add(new Order
+                {
+                    Date = item.Date,
+                    IDShip = item.IDShip,
+                    IDCustomer = item.IDCustomer,
+                    Status = item.Status
+                });
+                item.Date = DateTime.Now;
+            }
+            
+        }
+
+        public List<Order> GetOrder()
+        {
+            
+            return listoOrders.ToList();
+        }
+    }
+}
