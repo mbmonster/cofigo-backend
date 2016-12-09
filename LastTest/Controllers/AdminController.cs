964b35471d294c42847c45327f79a5cc2382d644
hoanglb <mr.bin.bkdn@gmail.com>
2016-12-09 21:11:54 +0700

    push list user

--- LastTest/Controllers/AdminController.cs
+++ LastTest/Controllers/AdminController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -19,6 +21,58 @@ namespace LastTest.Controllers
         {
             var listUser = db.Users.ToList();
             return View(listUser);
+        }
+        public ActionResult EditUser(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.SingleOrDefault(s => s.ID == id);
+            return View(user);
+        }
+
+        [HttpPost, ActionName("EditUser")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditUserPost(string id)
+        {
+            var userUpdate = db.Users.Find(id);
+            if (TryUpdateModel(userUpdate, "", new string[] { "ID", "Account", "Role", "DisplayName" }))
+            {
+                try
+                {
+                    db.Entry(userUpdate).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    ModelState.AddModelError("", "Error Save Data");
+                }
+            }
+            return RedirectToAction("ListUser");
+        }
+
+        public ActionResult DeleteUser(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+        [HttpPost, ActionName("DeleteUser")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteUserPost(string id)
+        {
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("ListUser");
         }
     }
 }
