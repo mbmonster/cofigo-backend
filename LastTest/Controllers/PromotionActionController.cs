932000e7ff8b8a0d4772ce295d965ce819c6b2bd
hoanglb <mr.bin.bkdn@gmail.com>
2016-12-22 23:21:13 +0700

    hoang lam xong promotion

---
+++ LastTest/Controllers/PromotionActionController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LastTest.Controllers
+{
+    public class PromotionActionController : Controller
+    {
+        CoffeeServicesEntities db = new CoffeeServicesEntities();
+        public ActionResult GetListProm()
+        {
+            var prom = from s in db.Promotions
+                        select s;
+            return View(prom);
+        }
+        public ActionResult AddProm()
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("AddProm")]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddProm(Promotion prom)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+
+                    db.Promotions.Add(prom);
+                    db.SaveChanges();
+                }
+            }
+            catch (RetryLimitExceededException)
+            {
+                ModelState.AddModelError("","Error save data");
+            }
+            var listProm = from s in db.Promotions
+                       select s;
+            return View("GetListProm", listProm);
+        }
+        public ActionResult EditProm(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Promotion prom = db.Promotions.SingleOrDefault(s => s.ID == id);
+            if (prom == null)
+            {
+                return HttpNotFound();
+            }
+            return View(prom);
+        }
+
+        [HttpPost, ActionName("EditProm")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditPromPost(int? id)
+        {
+            var promUpdate = db.Promotions.Find(id);
+            if (TryUpdateModel(promUpdate, "", new string[] { "Title", "Description", "Image", "Created", "Last" }))
+            {
+                try
+                {
+                    db.Entry(promUpdate).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    ModelState.AddModelError("", "Error Save Data");
+                }
+            }
+            return RedirectToAction("GetListProm");
+        }
+        public ActionResult DeleteProm(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Promotion prom = db.Promotions.SingleOrDefault(s => s.ID == id);
+            if (prom == null)
+            {
+                return HttpNotFound();
+            }
+            return View(prom);
+        }
+        [HttpPost, ActionName("DeleteProm")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeletePromPost(int? id)
+        {
+            Promotion prom = db.Promotions.Find(id);
+            db.Promotions.Remove(prom);
+            db.SaveChanges();
+            return RedirectToAction("GetListProm");
+        }
+    }
+}
---
+++ LastTest/Models/PromotionManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LastTest.Models
+{
+    public class PromotionManager:IPromotionManager
+    {
+        CoffeeServicesEntities db = new CoffeeServicesEntities();
+        List<Promotion> promotions = new List<Promotion>();
+
+        public PromotionManager()
+        {
+            var promotion = (from s in db.Promotions select s).ToList();
+            foreach (var item in promotion)
+            {
+                promotions.Add(new Promotion
+                {
+                    ID = item.ID,
+                    Title = item.Title,
+                    Description = item.Description,
+                    Image = item.Image,
+                    IDStore = item.IDStore,
+                    Created = item.Created,
+                    Last = item.Last,
+                    IDMenu = item.IDMenu
+                });
+            }
+        }
+        public List<Promotion> GetTopPromotion()
+        {
+            var pro = promotions.OrderByDescending(p => p.Created).Take(6).ToList();
+            return pro;
+        }
+    }
+}
